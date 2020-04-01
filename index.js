9f04dbeaf9eb85c299e425af9ea2bc661d25f89c
Abhinav <001sancheti@gmail.com>
2020-04-01 22:54:35 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
-import '../POC1/node_modules/@lion/input/lion-input.js';
-
+import '@lion/input/lion-input.js';
+import '@lion/input-amount/lion-input-amount.js';
+import '@lion/input-date/lion-input-date.js';
 const value = "Mayank";
 // Define a template function
 const myTemplate = () => html`<p>Hello ${value}</p>
-<lion-input name="firstName" placeholder="Name"></lion-input>
+<lion-input label="First Name" id="Name" placeholder="Name"></lion-input>
+<p>Last Name</p>
+<lion-input label="Last Name" id="Name" placeholder="Name"></lion-input>
+<lion-input-date label="Date of application"></lion-input-date>
 <button @click=${clickHandler}>Click Me!</button>
 
 `;
