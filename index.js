c1b7011ee10bdc82b6ead9b850b09108216e6247
mayankthakur007 <mayankthakur231@gmail.com>
2020-04-01 18:30:52 +0530

    Working case

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
 
+const value = "Mayank";
 // Define a template function
-const myTemplate = (name) => html`<p>Hello ${name}</p>`;
+const myTemplate = () => html`<p>Hello ${value}</p>
+Name
+<input type="text" name="Name" id="Name"></input>
+<button @click=${clickHandler}>Click Me!</button>
+`;
+const clickHandler = {
+    // handleEvent method is required.
+    handleEvent(e) { 
+        let name= document.getElementById("Name").value;
+      console.log(name);
+    },
+    // event listener objects can also define zero or more of the event 
+    // listener options: capture, passive, and once.
+    capture: true,
+  };
 
 // Render the template with some data
 render(myTemplate('lit-html'), document.body);
