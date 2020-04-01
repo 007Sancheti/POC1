e25ff7345bea2be103e9d68f73104ce8741f3308
mayankthakur007 <mayankthakur231@gmail.com>
2020-04-01 18:09:25 +0530

    node

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-// LitElement and html are the basic required imports
-import { LitElement, html, css } from "lit-element";
+import {html, render} from 'https://unpkg.com/lit-html?module';
 
-// Create a class definition for your component and extend the LitElement base class
-class BasicSetup extends LitElement {
-  // The render callback renders your element's template. This should be a pure function,
-  // it should always return the same template given the same properties. It should not perform
-  // any side effects such as setting properties or manipulating the DOM. See the updated
-  // or first-updated examples if you need side effects.
-  render() {
-    // Return the template using the html template tag. lit-html will parse the template and
-    // create the DOM elements
-    return html`
-      <div>Hello world!</div>
-    `;
-  }
-}
+// Define a template function
+const myTemplate = (name) => html`<p>Hello ${name}</p>`;
 
-// Register your element to custom elements registry, pass it a tag name and your class definition
-// The element name must always contain at least one dash
-customElements.define("basic-setup", BasicSetup);
+// Render the template with some data
+render(myTemplate('lit-html'), document.body);
